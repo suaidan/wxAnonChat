b44c00c1c3c8988fe3498bed08c2c7430893e041
suaidan <3402735171@qq.com>
2017-11-03 00:04:15 +0800

    change pages order

--- app.js
+++ app.js
@@ -6,26 +6,45 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
-    }
-  },
-  globalData:{
-    peopleId: new Date().getTime(),
+  globalData: {
+    // peopleId: new Date().getTime(),
+    usrname: "",
     userInfo: {}
+  },
+  getUserInfo:function(){
+    var app=getApp();
+    wx.getStorage({//获取微信用户名
+      key: 'userWxName',
+      success: function(res) {
+        console.log(res.data);
+        console.log(app);
+        app.globalData.usrname=res.data;
+        return;
+      },
+      fail:function(){
+        wx.getUserInfo({
+          success: function (res) {
+            var userInfo = res.userInfo;
+            var nickName = userInfo.nickName;
+            wx.setStorage({
+              key: 'usrInfor',
+              data: userInfo
+            })
+            wx.setStorage({
+              key: 'userWxName',
+              data: nickName
+            })
+          },
+          fail: function (res) {
+            console.log(res);
+          },
+          complete: function (res) {
+            console.log("get user's information finished");
+            console.log(res);
+          }
+        })
+      }
+    })
+    
   }
 })
