065e37970cda2cec53b0c08c9a2d5a505939481f
suaidan <3402735171@qq.com>
2017-11-03 00:01:46 +0800

    index page change

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,31 @@
 const app = getApp()
 import { formatTime } from '../../utils/util'
 var socket = require('../../utils/socket')
-app.getUserInfo();
 Page({
     data:{
-      list: []
+      list: [
+        {
+          id:1,
+          name:"wonder",
+          text:"hello,someone",
+          count:4,
+          update:"2017:10:23"
+        },
+        {
+          id: 1,
+          name: "wonder",
+          text: "hello,someone",
+          count: 4,
+          update: "2017:10:23"
+        },
+        {
+          id: 1,
+          name: "wonder",
+          text: "hello,someone",
+          count: 4,
+          update: "2017:10:23"
+        }
+      ]
     },
     goPage(e) {
         let newlist = this.data.list
@@ -13,28 +34,28 @@ Page({
         this.setData({
             list: newlist
         });
-        console.log(this.data)
+        console.log(this.data);
         wx.navigateTo({
             url: '../message/message?name='+e.currentTarget.dataset.name+"&id="+e.currentTarget.dataset.id
         })
     },
     onLoad(){
-      socket.initSocket();
-      var that = this;
-      wx.onSocketMessage(function (res) {
-        console.log(res.data); 
-        var data = JSON.parse(res.data);
-        if (data.cmd != "CMD" || data.subCmd != 'ROOMS')
-          return
-        data.rooms.forEach((room) => {
-          room.updated = formatTime(room.updated)
-        })
-        if (data.cmd == 'CMD' && data.subCmd == 'ROOMS') {
-          that.setData({
-            list: data.rooms
-          })
-        }
-        console.log(data);
-      })
+      // socket.initSocket();
+      // var that = this;
+      // wx.onSocketMessage(function (res) {
+      //   console.log(res.data); 
+      //   var data = JSON.parse(res.data);
+      //   if (data.cmd != "CMD" || data.subCmd != 'ROOMS')
+      //     return
+      //   data.rooms.forEach((room) => {
+      //     room.updated = formatTime(room.updated)
+      //   })
+      //   if (data.cmd == 'CMD' && data.subCmd == 'ROOMS') {
+      //     that.setData({
+      //       list: data.rooms
+      //     })
+      //   }
+      //   console.log(data);
+      // })
     }
 })
