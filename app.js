537c21d38ec0adace4a320932c646eba027c6ce6
suaidan <3402735171@qq.com>
2017-11-14 23:58:32 +0800

    change app onlanch function

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 //app.js
 App({
   onLaunch: function () {
+    var token=wx.getStorageSync("token")||"";
+    wx.connectSocket({
+      url: 'ws://127.0.0.1:8000',
+    })
+    wx.onSocketOpen(function(res){
+      if(token){
+        wx.sendSocketMessage({
+          data: token,
+        })
+      }else{
+        wx.sendSocketMessage({
+          data: "notoken"
+        })
+      }
+    })
+    wx.onSocketMessage(function(res){
+      if(res.ok){
+        //后台判断token有效,然后分为长期有效还是临时的，再采取相应的办法
+      }
+    })
     //调用API从本地缓存中获取数据
-    var timeNow=+new Date();
+    var timeNow=+new Date();  
     var storTime=wx.getStorageSync("invokeTime")||0;
     var invoked = wx.getStorageSync('invoked')||false;
     if(invoked&&(timeNow-storTime<1.8144E10)){//在本地存储一个月
